d7136893b22dcc721f11b61b5989a9a24c08e36e
Odin <odinuv@gmail.com>
2015-10-25 20:34:05 +0100

    initial import biggus dickus

--- script.R
+++ script.R
@@ -1,4 +1,11 @@
 data <- read.csv(file = "/data/in/tables/source.csv");
 
-# do something 
+for (column in colnames(data)) {
+    data[[column]] <- as.character(data[[column]])
+    data[[column]] <- gsub("cc", "c", data[[column]])
+    data[[column]] <- gsub("ss", "s", data[[column]])
+    data[[column]] <- gsub("c", "th", data[[column]])
+    data[[column]] <- gsub("s", "th", data[[column]])
+}
+
 write.csv(data, file = "/data/out/tables/result.csv", row.names = FALSE)
