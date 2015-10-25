30fb5be51182718eef5d101d8b86ef5b5f5c1c1e
Odin <odinuv@gmail.com>
2015-10-25 20:37:31 +0100

    initial import pointius pilate

--- script.R
+++ script.R
@@ -1,4 +1,6 @@
 data <- read.csv(file = "/data/in/tables/source.csv");
-
-# do something 
+for (column in colnames(data)) {
+    data[[column]] <- gsub("rr", "r", data[[column]])
+    data[[column]] <- gsub("r", "w", data[[column]])
+}
 write.csv(data, file = "/data/out/tables/result.csv", row.names = FALSE)
